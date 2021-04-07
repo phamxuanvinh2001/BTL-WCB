5e8eb8b378e6dbad153ea7c71b790045d98969dc
DinhHuyKhanh <khanhdinh141@gmail.com>
2021-04-07 12:05:50 +0700

    solve conflish

---
+++ WebVeTau/WebVeTau/Entity/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebVeTau.Entity
+{
+    public class User
+    {
+        private string fullName;
+        private string cccd;
+        private string tel;
+        private string email;
+        private string pass;
+        private string gioitinh;
+        private string ngaysinh;
+
+        public User(string fullName, string cccd, string tel, string email, string pass, string gioitinh, string ngaysinh)
+        {
+            this.fullName = fullName;
+            this.cccd = cccd;
+            this.email = email;
+            this.tel = tel;
+            this.pass = pass;
+            this.gioitinh = gioitinh;
+            this.ngaysinh = ngaysinh;
+        }
+    }
+}
